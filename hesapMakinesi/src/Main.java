d0f429467fa101168e178d816d05296a36ba0e1f
Enes Aksoy <enesaksoy9494@gmail.com>
2024-10-01 18:04:25 +0300

    hesap makinesi

---
+++ hesapMakinesi/src/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        hesapMakinesi hesapMakinesi=new hesapMakinesi();
+        int sonuç= hesapMakinesi.topla(5,6);
+        System.out.println(sonuç);
+    }
+}
---
+++ hesapMakinesi/src/hesapMakinesi.java
@@ -0,0 +1,14 @@
+public class hesapMakinesi {
+    public int topla(int sayı1,int sayı2){
+        return sayı1+sayı2;
+    }
+    public int çıkar(int sayı1,int sayı2){
+        return sayı1-sayı2;
+    }
+    public int böl(int sayı1,int sayı2){
+        return sayı1/sayı2;
+    }
+    public int çarp(int sayı1,int sayı2){
+        return sayı1*sayı2;
+    }
+}
